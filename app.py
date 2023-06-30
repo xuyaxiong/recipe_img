2c0a8b476befea763af2b81dec2cfbd2b1dc2934
xuyaxiong <xyxlindy@163.com>
2023-06-30 15:26:05 +0800

    修改

--- app.py
+++ app.py
@@ -1,87 +1,33 @@
-import requests
-from bs4 import BeautifulSoup
-import re
-import os
-import random
-import time
-from utils import add_watermark
+import tkinter as tk
+import tkinter.messagebox as msgbox
+from utils import download_recipe_imgs
 
-base_url = 'https://machtalk.xiachufang.com'
-search_url = base_url + "/search/?keyword="
+class Window(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("下厨房菜谱图片下载")
+        self.geometry("300x300")
+        self.resizable(False, False)
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36",
-    "Accept-Language": "en-US,en;q=0.9"
-}
+        self.recipe_label = tk.Label(self, text="菜谱名称：")
+        self.recipe_label.pack()
 
+        self.recipe_name_var = tk.StringVar()
+        self.recipe_name_entry = tk.Entry(
+            self, textvariable=self.recipe_name_var)
+        self.recipe_name_entry.pack()
 
-# 获取菜谱链接列表
-def parse_recipe_urls(keyword):
-    search = search_url + keyword
-    response = requests.get(search, headers=headers)
-    content = response.text
-    soup = BeautifulSoup(content, "html.parser")
-    recipe_addr_list = soup.select('.recipe-96-horizon')
-    return [base_url + addr.get('href') for addr in recipe_addr_list if re.search('/recipe/\d+\/$', addr.get('href')) is not None]
+        self.button = tk.Button(
+            self, text="下载", command=self.download)
+        self.button.pack()
 
-
-# 获取菜谱主图下载地址
-def get_main_img_url(recipe_url):
-    response = requests.get(recipe_url, headers=headers)
-    content = response.text
-    soup = BeautifulSoup(content, "html.parser")
-    return soup.select('.recipe-main')[0].find_all('img')[0].get('src')
+    
+    def download(self):
+        recipe_name = self.recipe_name_var.get()
+        download_recipe_imgs(recipe_name)
+        msgbox.showinfo("提示", "下载完成！")
 
 
-# 下载图片
-def download_image(url, file_path):
-    response = requests.get(url, stream=True)
-    if response.status_code == 200:
-        with open(file_path, 'wb') as file:
-            for chunk in response.iter_content(1024):
-                file.write(chunk)
-        print("图片下载成功：", file_path)
-    else:
-        print("图片下载失败")
-
-
-# 创建目录
-def create_directory(directory):
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-
-
-# 保存菜谱图片
-def download_recipe_imgs(recipe_name):
-    recipe_urls = parse_recipe_urls(recipe_name)
-    save_path = f'./菜谱图片/{recipe_name}'
-    create_directory(save_path)
-    for i, recipe_url in enumerate(recipe_urls[:5]):
-        main_img_url = get_main_img_url(recipe_url)
-        dest_path = f'{save_path}/{i}.jpg'
-        dest_path_mark = f'{save_path}/{i}_mark.jpg'
-        # 下载图片
-        download_image(main_img_url, dest_path)
-        # 添加水印
-        add_watermark(dest_path, dest_path_mark, 'watermark.png')
-
-    time_to_sleep = int(random.random() * 3)
-    time.sleep(time_to_sleep)
-
-
-# 获取菜谱名称
-def get_names(file_path):
-    f = open(file_path, "r", encoding='utf-8')
-    lines = f.readlines()
-    name_list = []
-    for line in lines:
-        new_line = line.strip().split('（')[0]
-        name_list.append(re.split('\d+', new_line)[0])
-    f.close()
-    return name_list
-
-
-if __name__ == '__main__':
-    name_list = get_names('./菜谱名称.txt')[:1]
-    for name in name_list:
-        download_recipe_imgs(name)
+if __name__ == "__main__":
+    window = Window()
+    window.mainloop()       
--- utils.py
+++ utils.py
@@ -1,5 +1,87 @@
+import requests
+from bs4 import BeautifulSoup
 import cv2
 import numpy as np
+import re
+import os
+import random
+import time
+
+base_url = 'https://machtalk.xiachufang.com'
+search_url = base_url + "/search/?keyword="
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36",
+    "Accept-Language": "en-US,en;q=0.9"
+}
+
+
+# 获取菜谱链接列表
+def parse_recipe_urls(keyword):
+    search = search_url + keyword
+    response = requests.get(search, headers=headers)
+    content = response.text
+    soup = BeautifulSoup(content, "html.parser")
+    recipe_addr_list = soup.select('.recipe-96-horizon')
+    return [base_url + addr.get('href') for addr in recipe_addr_list if re.search('/recipe/\d+\/$', addr.get('href')) is not None]
+
+
+# 获取菜谱主图下载地址
+def get_main_img_url(recipe_url):
+    response = requests.get(recipe_url, headers=headers)
+    content = response.text
+    soup = BeautifulSoup(content, "html.parser")
+    return soup.select('.recipe-main')[0].find_all('img')[0].get('src')
+
+
+# 下载图片
+def download_image(url, file_path):
+    response = requests.get(url, stream=True)
+    if response.status_code == 200:
+        with open(file_path, 'wb') as file:
+            for chunk in response.iter_content(1024):
+                file.write(chunk)
+        print("图片下载成功：", file_path)
+    else:
+        print("图片下载失败")
+
+
+# 创建目录
+def create_directory(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+
+# 保存菜谱图片
+def download_recipe_imgs(recipe_name):
+    recipe_urls = parse_recipe_urls(recipe_name)
+    save_path = f'./菜谱图片/{recipe_name}'
+    create_directory(save_path)
+    for i, recipe_url in enumerate(recipe_urls[:5]):
+        main_img_url = get_main_img_url(recipe_url)
+        dest_path = f'{save_path}/{i}.jpg'
+        dest_path_mark = f'{save_path}/{i}_mark.jpg'
+        # 下载图片
+        download_image(main_img_url, dest_path)
+        # 添加水印
+        add_watermark(dest_path, dest_path_mark, 'watermark.png')
+
+    time_to_sleep = int(random.random() * 3)
+    time.sleep(time_to_sleep)
+
+
+# 获取菜谱名称
+def get_names(file_path):
+    f = open(file_path, "r", encoding='utf-8')
+    lines = f.readlines()
+    name_list = []
+    for line in lines:
+        new_line = line.strip().split('（')[0]
+        name_list.append(re.split('\d+', new_line)[0])
+    f.close()
+    return name_list
+
+
 
 def add_watermark(src, dest, watermark):
     image = cv2.imdecode(np.fromfile(src, dtype=np.uint8), cv2.IMREAD_UNCHANGED)
@@ -28,3 +110,9 @@ def add_watermark(src, dest, watermark):
 
     # 保存到中文路径
     cv2.imencode('.jpg', image)[1].tofile(dest)
+
+
+if __name__ == '__main__':
+    name_list = get_names('./菜谱名称.txt')[:1]
+    for name in name_list:
+        download_recipe_imgs(name)
